88853120e837d08c70ba10720419a0227c59f9db
teemuvirkkala <virkkte@gmail.com>
2015-11-20 19:11:41 +0100

    Movement and penguin checking
    -Added basic movement functions called MovePeng* where * = NE, E, SE,
    SW, W, NW. Will move only the penguin with given ID.
    -Created CheckPeng for checking if there's a penguin on the given
    coordinates. Return 1 if the space is free and 0 if there's a penguin on
    given coordinates.
    -PosPeng works now for multiple penguins, where number of penguins is
    equal to PengRows. Will check if there's already a penguin in the same
    spot.
    -Updated DrawBoard to print out the multiple penguins.
    -Added TURNBYTURN_MODE to main.h to give the option to clear screen
    every DrawScreen() and wait for a button press from the user. This makes
    turns easier to follow, but there's still the option to print out
    everything (Windows only currently).

--- PengPos.c
+++ PengPos.c
@@ -2,24 +2,23 @@
 
 void PosPeng(int NumOfRows, int NumOfCols, int FishArray[NumOfRows][NumOfCols], int PengRows, int PengArray[PengRows][3]) {
 
-    int i,j;
+    int i,j,a;
     int found = 0;
 
-    for(i=0; i < NumOfRows; i++) {
-        for(j=0; j < NumOfCols; j++) {
-            if(FishArray[i][j] == 1) {
-                PengArray[0][0] = 1; //Player ID
-                PengArray[0][1] = i; //X coordinate for penguin
-                PengArray[0][2] = j; //Y coordinate for penguin
-                found = 1;
-                break;
+    for(a=0; a < PengRows; a++) {
+        for(i=0; i < NumOfRows; i++) {
+            for(j=0; j < NumOfCols; j++) {
+                if(FishArray[i][j] == 1 && CheckPeng(i, j, PengRows, PengArray)) {
+                    PengArray[a][0] = a+1; //Penguin ID
+                    PengArray[a][1] = i; //X coordinate for penguin
+                    PengArray[a][2] = j; //Y coordinate for penguin
+                    found = 1;
+                    break;
+                }
             }
+            if(found == 1)
+                break;
         }
-        if(found == 1) {
-            break;
-        }
-
+        found = 0;
     }
-
-
 }
--- main.h
+++ main.h
@@ -2,7 +2,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#ifdef __unix__
+# include <unistd.h>
+#elif defined _WIN32
+# include <windows.h>
+#define sleep(x) Sleep(1000 * x)
+#endif
+
+#define TURNBYTURN_MODE 1
+
+/*TURNBYTURN_MODE, Wait the player to press anything every DrawScreen() and clear the screen,
+easier to follow longer games, but still gives us the opportunity to see full output, windows only*/
+
 //Function prototypes
 void MapGenerator(int NumOfRows, int NumOfCols, int FishArray[NumOfRows][NumOfCols]);
 void DrawBoard(int NumOfRows, int NumOfCols, int FishArray[NumOfRows][NumOfCols], int PengRows, int PengArray[PengRows][3]);
 void PosPeng(int NumOfRows, int NumOfCols, int FishArray[NumOfRows][NumOfCols], int PengRows, int PengArray[PengRows][3]);
+int CheckPeng(int, int, int PengRows, int PengArray[PengRows][3]);
+
+//Movement function prototypes
+void MovePengNE(int, int, int NumOfRows, int NumOfCols, int FishArray[NumOfRows][NumOfCols], int PengRows, int PengArray[PengRows][3]);
+void MovePengE(int, int, int NumOfRows, int NumOfCols, int FishArray[NumOfRows][NumOfCols], int PengRows, int PengArray[PengRows][3]);
+void MovePengSE(int, int, int NumOfRows, int NumOfCols, int FishArray[NumOfRows][NumOfCols], int PengRows, int PengArray[PengRows][3]);
+void MovePengSW(int, int, int NumOfRows, int NumOfCols, int FishArray[NumOfRows][NumOfCols], int PengRows, int PengArray[PengRows][3]);
+void MovePengW(int, int, int NumOfRows, int NumOfCols, int FishArray[NumOfRows][NumOfCols], int PengRows, int PengArray[PengRows][3]);
+void MovePengNW(int, int, int NumOfRows, int NumOfCols, int FishArray[NumOfRows][NumOfCols], int PengRows, int PengArray[PengRows][3]);
