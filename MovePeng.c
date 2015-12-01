0cc7720bef76eb45c1d4c6cc346090c91d26a314
teemuvirkkala <virkkte@gmail.com>
2015-12-01 18:04:32 +0100

    Movement rework
    -Reworked the movement commands so we actually first check the whole
    move and then move if the move is possible.
    -Feedback to the player/playing algorithm still needs to be added.
    -After moving the original tile will now revert to 0.

--- MovePeng.c
+++ MovePeng.c
@@ -2,32 +2,29 @@
 
 /*Basic movement commands in order of NE,E,SE,SW,W,NW
 
--Currently only checking for Penguin ID and then move the Penguin for amount of Spaces
--Checking function CheckMove() checks that the tile isn't a 0 and that there are no penguins on the tile
--Feedback for the player if the move is invalid? Currently if the move isn't possible it's not completed (partial movement is possible)
--Amount of arguments? Do we need to pass FishArray for the check function?*/
+-Checking function CheckTile() checks that the tile isn't a 0 and that there are no penguins on the tile.
+-Checking function CheckMove() checks all the tiles before we actually move at all. If there's a penguin or a zero on the path we don't move at all!
+-Feedback for the player if the move is invalid?*/
 
 void MovePengNE(int ID, int Spaces, int NumOfRows, int NumOfCols, int FishArray[NumOfRows][NumOfCols], int AllPengs, int PengArray[AllPengs][3]) {
 
     int i, idRow, Row, Col;
 
     idRow = WhichPenguin(ID, AllPengs, PengArray); //Search for the right row in the array so we don't have to check it every incrementation of Spaces
+    Row = PengArray[idRow][1];
+    Col = PengArray[idRow][2];
 
-    for(i=0; i < Spaces; i++) {
-        Row = PengArray[idRow][1];
-        Col = PengArray[idRow][2];
-
-        if(PengArray[idRow][1] % 2) {
-            //Check if the move is possible before executing
-            if(CheckMove(--Row, ++Col, NumOfCols, FishArray, AllPengs, PengArray) == 1) {
+    if(CheckMove(idRow, 1, Spaces, Row, Col, NumOfCols, FishArray, AllPengs, PengArray)) {
+        FishArray[Row][Col] = 0;
+        for(i = 0; i < Spaces; i++) {
+            if(PengArray[idRow][1] % 2) {
                 (PengArray[idRow][1])--; //X -= 1
                 (PengArray[idRow][2])++; //Y += 1
+            } else {
+                (PengArray[idRow][1])--; //X -= 1
             }
-        } else {
-            if(CheckMove(--Row, Col, NumOfCols, FishArray, AllPengs, PengArray) == 1)
-                (PengArray[idRow][1])--; //Y += 1
         }
-    }
+    } //if the move fails inform player somehow?
 }
 
 
@@ -36,14 +33,12 @@ void MovePengE(int ID, int Spaces, int NumOfRows, int NumOfCols, int FishArray[N
     int i, idRow, Row, Col;
 
     idRow = WhichPenguin(ID, AllPengs, PengArray); //Search for the right row in the array so we don't have to check it every incrementation of Spaces
+    Row = PengArray[idRow][1];
+    Col = PengArray[idRow][2];
 
-    for(i=0; i < Spaces; i++) {
-        //Check if the move is possible before executing
-        Row = PengArray[idRow][1];
-        Col = PengArray[idRow][2];
-
-        if(CheckMove(Row, ++Col, NumOfCols, FishArray, AllPengs, PengArray) == 1)
-            (PengArray[idRow][2])++; //Y += 1
+    if(CheckMove(idRow, 2, Spaces, Row, Col, NumOfCols, FishArray, AllPengs, PengArray)) {
+        FishArray[Row][Col] = 0;
+        PengArray[idRow][2] += Spaces; //Y += Spaces
     }
 }
 
@@ -52,45 +47,41 @@ void MovePengSE(int ID, int Spaces, int NumOfRows, int NumOfCols, int FishArray[
     int i, idRow, Row, Col;
 
     idRow = WhichPenguin(ID, AllPengs, PengArray); //Search for the right row in the array so we don't have to check it every incrementation of Spaces
+    Row = PengArray[idRow][1];
+    Col = PengArray[idRow][2];
 
-    for(i=0; i < Spaces; i++) {
-        //Check if the move is possible before executing
-        Row = PengArray[idRow][1];
-        Col = PengArray[idRow][2];
-
-        if(PengArray[idRow][1] % 2) {
-            if(CheckMove(++Row, ++Col, NumOfCols, FishArray, AllPengs, PengArray) == 1) {
-                (PengArray[idRow][1])++;
-                (PengArray[idRow][2])++;
+    if(CheckMove(idRow, 3, Spaces, Row, Col, NumOfCols, FishArray, AllPengs, PengArray)) {
+        FishArray[Row][Col] = 0;
+        for(i = 0; i < Spaces; i++) {
+            if(PengArray[idRow][1] % 2) {
+                (PengArray[idRow][1])++; //X += 1
+                (PengArray[idRow][2])++; //Y += 1
+            } else {
+                (PengArray[idRow][1])++; //Y += 1
             }
-        } else {
-            if(CheckMove(++Row, Col, NumOfCols, FishArray, AllPengs, PengArray) == 1)
-                (PengArray[idRow][1])++;
         }
-    }
+    } //if the move fails inform player somehow?
 }
 
 void MovePengSW(int ID, int Spaces, int NumOfRows, int NumOfCols, int FishArray[NumOfRows][NumOfCols], int AllPengs, int PengArray[AllPengs][3]) {
 
-    int i,idRow, Row, Col;
+    int i, idRow, Row, Col;
 
     idRow = WhichPenguin(ID, AllPengs, PengArray); //Search for the right row in the array so we don't have to check it every incrementation of Spaces
+    Row = PengArray[idRow][1];
+    Col = PengArray[idRow][2];
 
-    for(i=0; i < Spaces; i++) {
-        //Check if the move is possible before executing
-        Row = PengArray[idRow][1];
-        Col = PengArray[idRow][2];
-
-        if(PengArray[idRow][1] % 2 == 0) {
-            if(CheckMove(++Row, --Col, NumOfCols, FishArray, AllPengs, PengArray) == 1) {
-                (PengArray[idRow][1])++;
-                (PengArray[idRow][2])--;
+    if(CheckMove(idRow, 4, Spaces, Row, Col, NumOfCols, FishArray, AllPengs, PengArray)) {
+        FishArray[Row][Col] = 0;
+        for(i = 0; i < Spaces; i++) {
+            if(PengArray[idRow][1] % 2 == 0) {
+                (PengArray[idRow][1])++; //X += 1
+                (PengArray[idRow][2])--; //Y -= 1
+            } else {
+                (PengArray[idRow][1])++; //Y += 1
             }
-        } else {
-            if(CheckMove(++Row, Col, NumOfCols, FishArray, AllPengs, PengArray) == 1)
-                (PengArray[idRow][1])++;
         }
-    }
+    } //if the move fails inform player somehow?
 }
 
 void MovePengW(int ID, int Spaces, int NumOfRows, int NumOfCols, int FishArray[NumOfRows][NumOfCols], int AllPengs, int PengArray[AllPengs][3]) {
@@ -98,14 +89,12 @@ void MovePengW(int ID, int Spaces, int NumOfRows, int NumOfCols, int FishArray[N
     int i, idRow, Row, Col;
 
     idRow = WhichPenguin(ID, AllPengs, PengArray); //Search for the right row in the array so we don't have to check it every incrementation of Spaces
+    Row = PengArray[idRow][1];
+    Col = PengArray[idRow][2];
 
-    for(i=0; i < Spaces; i++) {
-        //Check if the move is possible before executing
-        Row = PengArray[idRow][1];
-        Col = PengArray[idRow][2];
-
-        if(CheckMove(Row, --Col, NumOfCols, FishArray, AllPengs, PengArray) == 1)
-                (PengArray[idRow][2])--;
+    if(CheckMove(idRow, 5, Spaces, Row, Col, NumOfCols, FishArray, AllPengs, PengArray)) {
+        FishArray[Row][Col] = 0;
+        PengArray[idRow][2] -= Spaces;
     }
 }
 
@@ -114,20 +103,18 @@ void MovePengNW(int ID, int Spaces, int NumOfRows, int NumOfCols, int FishArray[
     int i, idRow, Row, Col;
 
     idRow = WhichPenguin(ID, AllPengs, PengArray); //Search for the right row in the array so we don't have to check it every incrementation of Spaces
+    Row = PengArray[idRow][1];
+    Col = PengArray[idRow][2];
 
-    for(i=0; i < Spaces; i++) {
-        //Check if the move is possible before executing
-        Row = PengArray[idRow][1];
-        Col = PengArray[idRow][2];
-
-        if(PengArray[idRow][1] % 2 == 0) {
-            if(CheckMove(--Row, --Col, NumOfCols, FishArray, AllPengs, PengArray) == 1) {
-                (PengArray[idRow][1])--;
-                (PengArray[idRow][2])--;
+    if(CheckMove(idRow, 6, Spaces, Row, Col, NumOfCols, FishArray, AllPengs, PengArray)) {
+        FishArray[Row][Col] = 0;
+        for(i = 0; i < Spaces; i++) {
+            if(PengArray[idRow][1] % 2 == 0) {
+                (PengArray[idRow][1])--; //X -= 1
+                (PengArray[idRow][2])--; //Y -= 1
+            } else {
+                (PengArray[idRow][1])--; //Y += 1
             }
-        } else {
-            if(CheckMove(--Row, Col, NumOfCols, FishArray, AllPengs, PengArray) == 1)
-                (PengArray[idRow][1])--;
         }
-    }
+    } //if the move fails inform player somehow?
 }
