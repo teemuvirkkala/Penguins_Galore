7fbe59feaade6222b3c9a3d0f21e9e8ddb218ffa
teemuvirkkala <virkkte@gmail.com>
2015-12-01 17:59:07 +0100

    InitArray()
    -Added InitArray() placeholder function for redundancy so nothing funny
    will happen with the memory.
    -Should be replaced with actual dynamic memory management in the future.

--- MapGenerator.c
+++ MapGenerator.c
@@ -1,5 +1,17 @@
 #include "main.h"
 
+//Init array is a placeholder function just so other functions work properly. This should be done with calloc instead.
+
+void InitArray(int Rows, int Cols, int a[Rows][Cols]) {
+    int i,j;
+
+    for(i = 0; i < Rows; i++) {
+        for(j = 0; j < Cols; j++) {
+            a[i][j] = 0;
+        }
+    }
+}
+
 void MapGenerator(int NumOfRows, int NumOfCols, int FishArray[NumOfRows][NumOfCols]) {
 
     srand(time(NULL));         // Generator of number of fishes
