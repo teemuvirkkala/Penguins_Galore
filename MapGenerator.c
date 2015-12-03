cdbbfe3d56e9a4e73075a5b4b49f2fc00eb26f27
MikelThief <m.bator2@gmail.com>
2015-12-03 16:45:49 +0100

    Enchanced randomizer
    Now creation of floes goes as following:
    50% chance of 1-fish floe
    30% chance of 2-fish floe
    20% chance of 3-fish floe

--- MapGenerator.c
+++ MapGenerator.c
@@ -3,11 +3,21 @@
 void MapGenerator(int NumOfRows, int NumOfCols, int FishArray[NumOfRows][NumOfCols]) {
 
     srand(time(NULL));         // Generator of number of fishes
-    int i,j;
+    int i,j,RandomNumber;
 
     for (i=1;i<NumOfRows-1;i++) {
-        for(j=1;j<NumOfCols-1;j++) {
-                FishArray[i][j]=((rand()%3)+1);
+        for(j=1;j<NumOfCols-1;j++) {                //
+                RandomNumber = rand() % 100;
+    RandomNumber -= 20;
+        if (RandomNumber < 0)
+  FishArray[i][j]=2;
+    else {
+  RandomNumber -= 30;
+  if (RandomNumber < 0)
+    FishArray[i][j]=3;
+  else
+    FishArray[i][j]=1;
+}
             }
     }
 }
