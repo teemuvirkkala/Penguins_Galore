12d1601362e06db6498a0dc563815a301a6e85c0
teemuvirkkala <virkkte@gmail.com>
2015-12-08 16:41:12 +0100

    MapGenerator's code cleaned up
    -MapGenerator's code is easier to read now.

--- MapGenerator.c
+++ MapGenerator.c
@@ -2,22 +2,22 @@
 
 void MapGenerator(int NumOfRows, int NumOfCols, int FishArray[NumOfRows][NumOfCols]) {
 
-    srand(time(NULL));         // Generator of number of fishes
+    srand(time(NULL));  // Generator of number of fishes
     int i,j,RandomNumber;
 
-    for (i=1;i<NumOfRows-1;i++) {
-        for(j=1;j<NumOfCols-1;j++) {                //
-                RandomNumber = rand() % 100;
-    RandomNumber -= 20;
-        if (RandomNumber < 0)
-  FishArray[i][j]=2;
-    else {
-  RandomNumber -= 30;
-  if (RandomNumber < 0)
-    FishArray[i][j]=3;
-  else
-    FishArray[i][j]=1;
-}
+    for (i = 1; i < NumOfRows - 1; i++) {
+        for(j = 1; j < NumOfCols - 1; j++) {
+            RandomNumber = rand() % 100;
+            RandomNumber -= 20;
+            if (RandomNumber < 0)
+                FishArray[i][j] = 2;
+            else {
+                RandomNumber -= 30;
+                if (RandomNumber < 0)
+                    FishArray[i][j] = 3;
+                else
+                    FishArray[i][j] = 1;
             }
+        }
     }
 }
