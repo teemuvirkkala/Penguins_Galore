2f54d2c4b5f113663939ddcb5f0b10d3cf5e6f8c
teemuvirkkala <virkkte@gmail.com>
2015-12-08 16:39:46 +0100

    InitArray removed
    -InitArray was removed because it did exactly the same thing as
    MapCleaner.
    -MapCleaner's code is now easier to read.

--- MapCleaner.c
+++ MapCleaner.c
@@ -1,25 +1,11 @@
 #include "main.h"
 
 void MapCleaner(int NumOfRows, int NumOfCols, int FishArray[NumOfRows][NumOfCols]) {
-
-    srand(time(NULL));         // Generator of number of fishes
     int i,j;
 
-    for (i=0;i<NumOfRows;i++) {
-        for(j=0;j<NumOfCols;j++) {
-                FishArray[i][j]=0;
-            }
-    }
-}
-
-//Init array is a placeholder function just so other functions work properly. This should be done with calloc instead.
-
-void InitArray(int Rows, int Cols, int a[Rows][Cols]) {
-    int i,j;
-
-    for(i = 0; i < Rows; i++) {
-        for(j = 0; j < Cols; j++) {
-            a[i][j] = 0;
+    for (i = 0; i < NumOfRows; i++) {
+        for(j = 0; j < NumOfCols; j++) {
+            FishArray[i][j] = 0;
         }
     }
 }
