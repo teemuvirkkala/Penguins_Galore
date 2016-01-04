a17809c1b30dd055c2b6292b1a1af575c427994a
teemuvirkkala <virkkte@gmail.com>
2016-01-04 14:58:06 +0100

    Score() changed
    -Changed so Score() that it will only increment the score and return the
    current score. This makes loading a game from a file easier.

--- Score.c
+++ Score.c
@@ -1,15 +1,7 @@
 #include "main.h"
 
-int Score(int AllPengs, int idRow, int Row, int Col, int NumOfCols, int FishArray[][NumOfCols], int PengArray[][3]) {
+int Score(int score, int Row, int Col, int NumOfCols, int FishArray[][NumOfCols]) {
 
-    static int ScoreP1, ScoreP2;
-
-    if(idRow % 2 == 0) {
-        ScoreP1 += CheckBoard(Row, Col, NumOfCols, FishArray);
-        return ScoreP1;
-    }
-    else {
-        ScoreP2 += CheckBoard(Row, Col, NumOfCols, FishArray);
-        return ScoreP2;
-    }
+    score += CheckBoard(Row, Col, NumOfCols, FishArray);
+    return score;
 }
